490ea0959df5f6dc87f620a7069ef9681fab2f42
HiepTD <hiepqd02@gmail.com>
2023-10-23 09:33:54 +0700

    a

--- genCrossWord.py
+++ genCrossWord.py
@@ -83,12 +83,14 @@ def main():
                     title_element, 100, 100).click().perform()
 
                 # Random layout
-                options = driver.find_elements(By.XPATH, '//*[@id=":r5:"]/li')
-                option = random.choice(options)
-                if option != options[0]:
+                random_index = random.choice([0, 1, 2, 3, 4, 5])
+                if random_index != 0:
                     layout_tab = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
                         (By.XPATH, '//*[@id="CROSSWORD"]/div[2]/div/div[2]/div[1]/div[2]/div')))
                     layout_tab.click()
+                    options = driver.find_elements(
+                        By.XPATH, '//*[@id=":r5:"]/li')
+                    option = options[random_index]
                     driver.execute_script(
                         "arguments[0].scrollIntoView(true);", option)
                     option.click()
